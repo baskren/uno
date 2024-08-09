03eb1e58e4aaef0f3711eaff0de314c56cefb2a3
Youssef Victor <youssefvictor00@gmail.com>
2024-08-09 18:55:56 +0300

    chore: Fix for Hyperlink

--- src/Uno.UI/UI/Xaml/Window/WindowManagerInterop.wasm.cs
+++ src/Uno.UI/UI/Xaml/Window/WindowManagerInterop.wasm.cs
@@ -1019,6 +1019,12 @@ namespace Uno.UI.Xaml
 		internal static void SetIsFocusable(IntPtr htmlId, bool isFocusable)
 			=> NativeMethods.SetIsFocusable(htmlId, isFocusable);
 
+		internal static UIElement TryGetElementInCoordinate(Point point)
+		{
+			var htmlId = NativeMethods.GetElementInCoordinate(point.X, point.Y);
+			return UIElement.GetElementFromHandle(htmlId);
+		}
+
 		internal static partial class NativeMethods
 		{
 			[JSImport("globalThis.Uno.UI.WindowManager.current.arrangeElementNativeFast")]
@@ -1117,6 +1123,9 @@ namespace Uno.UI.Xaml
 
 			[JSImport("globalThis.Uno.UI.WindowManager.current.setIsFocusable")]
 			internal static partial void SetIsFocusable(nint htmlId, bool isFocusable);
+
+			[JSImport("globalThis.Uno.UI.WindowManager.current.getElementInCoordinate")]
+			internal static partial IntPtr GetElementInCoordinate(double x, double y);
 		}
 	}
 }
